180128c88009281437afbd1b07ea16e6b6525d6a
Austin Shook <austinleeshook@gmail.com>
2023-08-11 11:10:36 -0400

    Updated src/utils files

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,24 +1,82 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
-const { signToken } = require('../utils/auth');
+import { gql } from '@apollo/client';
 
-const resolvers = {
+export const ADD_USER = gql`
+mutation AddUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
 
-  Query: {
-    users: async () => {
-      
-    },
-  },
+}
+  }
+}
+`;
+
+export const LOGIN = gql`
+mutation Login($username: String!, $password: String!) {
+  login(username: $username, password: $password) {
+    token
+    user {
+      _id
+      username
+    }
+  }
+}
+}
+`;
 
 
 
-  Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      
-    },
-    
+export const ADD_RECIPE= gql`
+mutation AddRecipe($title: String, $ingredients: [String], $instructions: String) {
+  addRecipe(title: $title, ingredients: $ingredients, instructions: $instructions) {
+    _id
+    title
+    ingredients
+    instructions
+    createdAt
+    comments {
+      _id
+      commentText
+      commentAuthor
+      createdAt
+    }
+  }
+}
+`;
 
-  },
-};
+export const ADD_COMMENT = gql`
+mutation AddComment($recipeId: ID!, $commentText: String, $commentAuthor: String) {
+  addComment(recipeId: $recipeId, commentText: $commentText, commentAuthor: $commentAuthor) {
+    _id
+    comments {
+      commentText
+      commentAuthor
+      createdAt
+    }
+  }
+}
+`;
 
-module.exports = resolvers;
+
+export const REMOVE_RECIPE = gql`
+mutation RemoveRecipe($recipeId: ID!) {
+  removeRecipe(recipeId: $recipeId) {
+    _id
+  }
+}
+`;
+
+
+
+export const REMOVE_COMMENT = gql`
+mutation RemoveComment($recipeId: ID!, $commentId: ID!) {
+  removeComment(recipeId: $recipeId, commentId: $commentId) {
+    _id
+    comments {
+      _id
+    }
+  }
+}
+`;
