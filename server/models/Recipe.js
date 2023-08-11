ad92a5fd290242110574abd73be33be7c2dd8911
Justin Martinez <jgm@Justins-MBP.attlocal.net>
2023-08-11 11:04:29 -0400

    models changed and content added

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,13 +1,58 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const recipeSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  ingredients: [{ type: String, required: true }],
-  instructions: { type: String, required: true },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const recipeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 100,
+    },
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+    ingredients: [{ 
+      type: String, 
+      required: true, 
+      minlength: 1,
+      maxlength: 280 
+    }],
+
+    instructions: { 
+      type: String, 
+      required: true, 
+      minlength: 1,
+      maxlength: 1000
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+
+    comments: [
+      {
+        commentText: {
+          type: String,
+          required: true,
+          minlength: 1,
+          maxlength: 280,
+        },
+        commentAuthor: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (timestamp) => dateFormat(timestamp),
+        },
+      },
+    ],
+  },
+  { timestamps: true } // Adds createdAt and updatedAt automatically
+);
+
+const Recipe = model('Recipe', recipeSchema);
 
 module.exports = Recipe;
